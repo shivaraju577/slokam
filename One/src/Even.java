f01e828eb7274509c00a63d759304598819985a3
kasiraju <kasiraju@LAPTOP-NUFP5HIR>
2020-07-09 13:04:05 +0530

    even program

---
+++ One/src/Even.java
@@ -0,0 +1,14 @@
+
+public class Even {
+	public static void main(String[] args) {
+		int n= 0;
+		if (n%2==0) {
+			System.out.println(" Enen Number");
+		}
+		else {
+				System.out.println(" Odd Number");
+			
+		}
+	}
+
+}
